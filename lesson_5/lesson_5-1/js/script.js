141103dcbb0062f63dbad5690234b09879222323
Maks Safonov <safonovmaks1@yandex.ru>
2019-03-08 10:09:16 +0300

    lesson_5--correction

--- lesson_5/lesson_5-1/js/script.js
+++ lesson_5/lesson_5-1/js/script.js
@@ -1,10 +1,13 @@
 let menu = document.querySelector('.menu'),
+    menuItem = document.getElementsByClassName('menu-item'),
     li = document.createElement('li'),
     title = document.getElementById('title'),
     column = document.getElementsByClassName('column'),
     adv = document.getElementsByClassName('adv'),
     box = document.getElementById('prompt'),
     question = prompt('Как вы относитесь к технике apple?', '');
+
+menu.insertBefore(menuItem[2], menuItem[1]);
 
 li.classList.add('menu-item');
 li.innerText = 'Пятый пункт';
