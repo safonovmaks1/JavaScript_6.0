382bc7b1c80f7fa3c5b06d3940cac9349d71787b
Maks Safonov <safonovmaks1@yandex.ru>
2019-03-21 17:29:54 +0300

    lesson_14

--- lesson_14/Homework/js/script.js
+++ lesson_14/Homework/js/script.js
@@ -1,3 +1,16 @@
-$(document).ready(function() {
-    
+$(function() {
+
+    function showModal() {
+        $('.overlay').fadeTo(1000, 1);
+        $('.modal').slideDown(2000);
+    }
+
+    function closeModal() {
+        $('.overlay').fadeOut(2000, 0);
+        $('.modal').slideUp(600);
+    }
+
+    $('a[href="#sheldure"], .main_btna, .contact').on('click', showModal);
+    $('.close').on('click', closeModal);
+
 });
